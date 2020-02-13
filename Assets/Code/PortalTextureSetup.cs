565eb505615b9e91d93069d83e2bb040426a5d27
MrSpeedy68 <47033681+MrSpeedy68@users.noreply.github.com>
2020-02-13 23:54:50 +0000

    Portal Camera Working

---
+++ Assets/Code/PortalTextureSetup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour
+{
+    public Camera Camera_B;
+    public Material CameraTexture_B;
+
+    // Update is called once per frame
+    void Start()
+    {
+        if (Camera_B.targetTexture != null)
+        {
+            Camera_B.targetTexture.Release();
+        }
+        Camera_B.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        CameraTexture_B.mainTexture = Camera_B.targetTexture;
+    }
+}
