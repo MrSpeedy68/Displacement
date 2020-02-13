d0302812ae810373936af226bbceb7c096dbe83c
Iodine <sjdsoundworks@gmail.com>
2020-02-13 21:26:55 +0000

    character implemented

---
+++ Assets/Code/FPSCameraLook.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+//Help from: https://github.com/jiankaiwang/FirstPersonController
+public class FPSCameraLook : MonoBehaviour
+{
+
+    public float lookSpeed = 5f;
+    public float camSmoothing = 1f; //must be minimum 1f as it multiplies lookspeed
+    private GameObject player;
+    private Vector2 mouseLook;
+    private Vector2 smoothVe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 delta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        delta = Vector2.Scale(delta, new Vector2(lookSpeed * camSmoothing, lookSpeed * camSmoothing));
+        smoothVe.x = Mathf.Lerp(smoothVe.x, delta.x, 1f / camSmoothing);
+        smoothVe.y = Mathf.Lerp(smoothVe.y, delta.y, 1f / camSmoothing);
+        mouseLook += smoothVe;
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
+    }
+}
---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float mvSpeed = 5f;
+    public float sprintSpeed = 7f;
+    private Rigidbody rb;
+    private float forward;
+    private float strafe;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float tempSpeed;
+        if (Input.GetButton("Sprint"))
+        {
+            tempSpeed = sprintSpeed;
+        }
+        else
+        {
+            tempSpeed = mvSpeed;
+        }
+        forward = Input.GetAxis("Vertical") * tempSpeed * Time.deltaTime;
+        strafe = Input.GetAxis("Horizontal") * tempSpeed * Time.deltaTime;
+        transform.Translate(strafe, 0f, forward);
+
+        Debug.Log(tempSpeed);
+        //Dev debug unlock mouse
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
