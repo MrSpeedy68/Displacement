cd31d2971d1016da3f948351dbff7d9be52a0e3f
MrSpeedy68 <47033681+MrSpeedy68@users.noreply.github.com>
2020-02-14 01:03:12 +0000

    Portals wont teleport god knows why
    nut wurking uwu

---
+++ Assets/PortalTeleporter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleporter : MonoBehaviour
+{
+    public Transform player;
+    public Transform reciever;
+
+    private bool playerIsOverlapping = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //dotProduct is the Cos angle between the portal and player
+        if (playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            //If this is true it means the player has moved through the portal
+            if (dotProduct < 0f)
+            {
+                //Teleport player and rotate him 180 to walk out the other side
+                float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
+                rotationDiff += 180;
+                player.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                player.position = reciever.position + positionOffset;
+            }
+        }
+    }
+
+    //Is the player colliding with the portal
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = true;
+        }
+    }
+
+    //Check that the player has left the portal
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+        }
+    }
+}
