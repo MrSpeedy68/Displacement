8fdc3db0ac1a6996c5aae582e35440d6464e7c5c
MrSpeedy68 <47033681+MrSpeedy68@users.noreply.github.com>
2020-02-18 03:01:56 +0000

    Open Door With Lever Done Like A Boss
    Absolute gigabrain operator stonky here turned over 70 lines of useless code that couldnt even run an animatrion into smooth 19 lines that do it all. Who needs sleep when u can read Unity scripts.

---
+++ Assets/Code/DoorController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        Animator anim = GetComponent<Animator>();
+        if(Input.GetButtonDown("Use"))
+        {
+            anim.SetTrigger("OpenClose");
+        }
+    }
+}
---
+++ Assets/LeverController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverController : MonoBehaviour
+{
+
+    private void OnTriggerStay(Collider other)
+    {
+        GameObject Door = GameObject.Find("SD_Env_Door_02");
+        Animator doorAnim = Door.GetComponent<Animator>();
+        Animator leverAnim = GetComponent<Animator>();
+        if (Input.GetButtonDown("Use"))
+        {
+            leverAnim.SetTrigger("UseLever");
+            doorAnim.SetTrigger("OpenClose");
+        }
+    }
+}
