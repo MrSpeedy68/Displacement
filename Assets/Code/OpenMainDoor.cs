acc3239b39b3aa3cd7a88cea1e565ac57938c6b0
MrSpeedy68 <47033681+MrSpeedy68@users.noreply.github.com>
2020-02-20 00:53:38 +0000

    Working Gems But u need to throw them at the alter

---
+++ Assets/Code/OpenMainDoor.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMainDoor : MonoBehaviour
+{
+    public GameObject RightDoor;
+    public GameObject LeftDoor;
+
+    public GameObject Gem1;
+    public GameObject Gem2;
+    public GameObject Gem3;
+    public GameObject Candle;
+    private int smooth = 1;
+
+    /*
+        private void OnTriggerStay(Collider other)
+        {
+        }
+        */
+
+    private void LateUpdate()
+    {
+        float distGem1 = Vector3.Distance(Candle.transform.position, Gem1.transform.position);
+        float distGem2 = Vector3.Distance(Candle.transform.position, Gem2.transform.position);
+        float distGem3 = Vector3.Distance(Candle.transform.position, Gem3.transform.position);
+
+
+
+        if (distGem1 <= 5f && distGem2 <= 5f && distGem3 <= 5f)
+        {
+            OpenDoors();
+        }
+        if (distGem1 <= 5f)
+        {
+            Gem1.transform.position = Vector3.MoveTowards(Gem1.transform.position, new Vector3(52.511f, -7.5f, 91.243f), Time.deltaTime * smooth);
+            Gem1.GetComponent<ThrowObject>().enabled = false;
+        }
+        if (distGem2 <= 5f)
+        {
+            Gem2.transform.position = Vector3.MoveTowards(Gem2.transform.position, new Vector3(51.646f, -7.5f, 93.406f), Time.deltaTime * smooth);
+            Gem2.GetComponent<ThrowObject>().enabled = false;
+        }
+        if (distGem3 <= 5f)
+        {
+            Gem3.transform.position = Vector3.MoveTowards(Gem3.transform.position, new Vector3(53.373f, -7.5f, 93.391f), Time.deltaTime * smooth);
+            Gem3.GetComponent<ThrowObject>().enabled = false;
+        }
+    }
+
+    
+    void OpenDoors ()
+    {
+        Animator rightDoorAnim = RightDoor.GetComponent<Animator>();
+        Animator leftDoorAnim = LeftDoor.GetComponent<Animator>();
+
+            rightDoorAnim.SetTrigger("OpenRight");
+            leftDoorAnim.SetTrigger("OpenLeft"); 
+    }
+}
--- Assets/OpenMainDoor.cs
+++ Assets/OpenMainDoor.cs
@@ -2,48 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpenMainDoor : MonoBehaviour
+public class OpenMainDoors : MonoBehaviour
 {
-    public GameObject RightDoor;
-    public GameObject LeftDoor;
+    public GameObject RightMainDoor;
+    public GameObject LeftMainDoor;
 
-    public GameObject Gem1;
-    public GameObject Gem2;
-    public GameObject Gem3;
-    private int smooth = 5;
+    public GameObject GemKey1;
+    public GameObject GemKey2;
+    public GameObject GemKey3;
+    public GameObject Candle;
+    private int smoothTime = 1;
 
-    /*
-        private void OnTriggerStay(Collider other)
-        {
-        }
-        */
 
     private void LateUpdate()
     {
-        float distGem1 = Vector3.Distance(gameObject.transform.position, Gem1.transform.position);
-        float distGem2 = Vector3.Distance(gameObject.transform.position, Gem2.transform.position);
-        float distGem3 = Vector3.Distance(gameObject.transform.position, Gem3.transform.position);
+        float distGem1 = Vector3.Distance(Candle.transform.position, GemKey1.transform.position);
+        float distGem2 = Vector3.Distance(Candle.transform.position, GemKey2.transform.position);
+        float distGem3 = Vector3.Distance(Candle.transform.position, GemKey3.transform.position);
+
+
 
         if (distGem1 <= 5f && distGem2 <= 5f && distGem3 <= 5f)
         {
-            OpenDoors();
+            OpenBothDoors();
+        }
+        if (distGem1 <= 5f)
+        {
+            GemKey1.transform.position = Vector3.MoveTowards(GemKey1.transform.position, new Vector3(52.511f, -7.5f, 91.243f), Time.deltaTime * smoothTime);
+            GemKey1.GetComponent<ThrowObject>().enabled = false;
+        }
+        if (distGem2 <= 5f)
+        {
+            GemKey2.transform.position = Vector3.MoveTowards(GemKey2.transform.position, new Vector3(51.646f, -7.5f, 93.406f), Time.deltaTime * smoothTime);
+            GemKey2.GetComponent<ThrowObject>().enabled = false;
+        }
+        if (distGem3 <= 5f)
+        {
+            GemKey3.transform.position = Vector3.MoveTowards(GemKey3.transform.position, new Vector3(53.373f, -7.5f, 93.391f), Time.deltaTime * smoothTime);
+            GemKey3.GetComponent<ThrowObject>().enabled = false;
         }
     }
 
-    /*
-    private void OnTriggerStay(Collider other)
+    
+    void OpenBothDoors ()
     {
-        if()
-        {
-            OpenDoors();
-        }
-        //Gem1.transform.position = Vector3.MoveTowards(other.transform.position, new Vector3(52, -7, 91), Time.deltaTime * smooth);
-    }
-    */
-    void OpenDoors ()
-    {
-        Animator rightDoorAnim = RightDoor.GetComponent<Animator>();
-        Animator leftDoorAnim = LeftDoor.GetComponent<Animator>();
+        Animator rightDoorAnim = RightMainDoor.GetComponent<Animator>();
+        Animator leftDoorAnim = LeftMainDoor.GetComponent<Animator>();
 
             rightDoorAnim.SetTrigger("OpenRight");
             leftDoorAnim.SetTrigger("OpenLeft"); 
