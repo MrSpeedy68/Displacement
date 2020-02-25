bf43d8b708a584745d464915e30b382220c2acc3
MrSpeedy68 <47033681+MrSpeedy68@users.noreply.github.com>
2020-02-25 20:54:49 +0000

    Level 1 platform done

---
+++ Assets/Code/MovingPlatformLever.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformLever : MonoBehaviour
+{
+    public GameObject Platform;
+
+    private void OnTriggerStay(Collider other)
+    {
+        Animator doorAnim = Platform.GetComponent<Animator>();
+        Animator leverAnim = GetComponent<Animator>();
+        if (Input.GetButtonDown("Use"))
+        {
+            leverAnim.SetTrigger("UseLever");
+            doorAnim.SetTrigger("Move");
+        }
+    }
+}
