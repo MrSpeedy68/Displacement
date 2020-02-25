a81ee386d3583bdeb5402f1537d8f0d6535405e3
Iodine <sjdsoundworks@gmail.com>
2020-02-25 20:20:38 +0000

    menu and portal ball updates

---
+++ Assets/Code/MainMenuButtonScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuButtonScript : MonoBehaviour
+{
+
+    private Button b;
+
+    public void GoToGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
