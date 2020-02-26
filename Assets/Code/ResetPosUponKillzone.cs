a6df0c8a3caf57b99bdc4a68abaff84b7550ce1f
Iodine <sjdsoundworks@gmail.com>
2020-02-26 22:23:27 +0000

    FIXED SCRIPT LOL

--- Assets/Code/ResetPosUponKillzone.cs
+++ Assets/Code/ResetPosUponKillzone.cs
@@ -7,14 +7,18 @@ public class ResetPosUponKillzone : MonoBehaviour
     private void Start()
     {
         resetPos = new GameObject("ResetPos").transform;
+        resetPos.position = transform.position;
+        resetPos.rotation = transform.rotation;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Killzone")
         {
+            if (tag == "Player") GetComponent<CharacterController>().enabled = false;
             transform.position = resetPos.position;
             transform.rotation = resetPos.rotation;
+            if (tag == "Player") GetComponent<CharacterController>().enabled = true;
         }
     }
 }
