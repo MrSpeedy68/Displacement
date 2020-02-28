c32ed5dc78c1339d26d0bd42077bf8a585e3d55b
Iodine <sjdsoundworks@gmail.com>
2020-02-28 20:20:09 +0000

    added lever highlighting to the lever-door prefab

---
+++ Assets/LeverHighlight.cs
@@ -0,0 +1,28 @@
+﻿using cakeslice;
+using UnityEngine;
+
+public class LeverHighlight : MonoBehaviour
+{
+
+    public Outline outl;
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+
+        if (dist < 5f)
+        {
+            outl.enabled = true;
+        }
+        else
+        {
+            outl.enabled = false;
+        }
+    }
+}
