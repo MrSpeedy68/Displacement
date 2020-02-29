d0e976cf491c4b2af75ddf64dbb065360adafb2e
Iodine <sjdsoundworks@gmail.com>
2020-02-29 14:09:58 +0000

    portal shooter enable volumes at portal cells

---
+++ Assets/EnablePortalShoot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class EnablePortalShoot : MonoBehaviour
+{
+
+    private PortalShooter sht;
+
+    private void Start()
+    {
+        Camera.main.GetComponent<PortalShooter>().enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Camera.main.GetComponent<PortalShooter>().enabled = true;
+        }
+    }
+}
