a6e063624d26fe39c12afb0fcdfa7f2f1ad24e55
Iodine <sjdsoundworks@gmail.com>
2020-02-29 13:55:05 +0000

    intro cutscene has music and a skip button now

---
+++ Assets/SceneSkipScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSkipScript : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
