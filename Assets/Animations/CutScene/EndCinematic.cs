dccae5cfa2acf0a2e1cfa512d6ae5fd27f1aa921
MrSpeedy68 <mrspeedygamer123@gmail.com>
2020-05-01 01:45:38 +0100

    Final Cinematic.

---
+++ Assets/Animations/CutScene/EndCinematic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndCinematic : MonoBehaviour
+{
+    public GameObject playerCam;
+    public GameObject cam2;
+    public GameObject Boat;
+    public GameObject Rouge;
+    public GameObject Wizard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(Cinematic());
+    }
+
+    IEnumerator Cinematic()
+    {
+        Animator BoatAnim = Boat.GetComponent<Animator>();
+        Animator RougeAnim = Rouge.GetComponent<Animator>();
+        Animator WizardAnim = Wizard.GetComponent<Animator>();
+        RougeAnim.enabled = false;
+        Wizard.SetActive(true);
+        playerCam.SetActive(false);
+        cam2.SetActive(true);
+        BoatAnim.SetTrigger("Sail");
+        
+        yield return new WaitForSeconds(1);
+        WizardAnim.enabled = false;
+        yield return new WaitForSeconds(35);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
